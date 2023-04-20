0a30f8f781340b2c962e9dbfa7fb18aae525625b
pullamsettymaanasa <130460207+pullamsettymaanasa@users.noreply.github.com>
2023-04-20 10:20:29 +0530

    Add files via upload

---
+++ pythagorean cw7.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+void pythagoreanTriplets(int limit)
+{
+ 
+    int a, b, c = 0;
+ 
+
+    int m = 2;
+    while (c < limit) {
+ 
+        for (int n = 1; n < m; ++n) {
+
+            a = m * m - n * n;
+            b = 2 * m * n;
+            c = m * m + n * n;
+ 
+            if (c > limit)
+                break;
+ 
+            printf("%d %d %d\n", a, b, c);
+        }
+        m++;
+    }
+}
+ 
+
+int main()
+{
+    int limit = 10;
+    pythagoreanTriplets(limit);
+    return 0;
+}
